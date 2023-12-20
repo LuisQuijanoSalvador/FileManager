1b1ea0f4d65fcf23d5a03e2d38fe1dd9677bfb33
LuisQuijanoSalvador <luis.quijano@hardnetconsulting.com>
2023-12-20 04:29:45 -0500

    Facturacion

--- app/Clases/Funciones.php
+++ app/Clases/Funciones.php
@@ -74,7 +74,7 @@ class Funciones
         // dd($jsonData);
         $respuesta = $client->request('POST', 'https://int.sendaefact.pe/webservice/emitir_dcto_cobranza', [
         'headers' => [
-                        'Authorization' => 'Bearer token de seguridad',
+                        'Authorization' => 'Bearer HfcH40sn5PFtDsN0eGaLTOBXb46Zf5C3I8sym7NESx1ZlAvNnIkpAlzb11Nd',
                         'Content-Type' => 'application/json',
                     ],
         'body' => $jsonData,
